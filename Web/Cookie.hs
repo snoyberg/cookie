41b2fc42454c638156fd6a8794eff06ab610bd5c
Michael Snoyman <michael@snoyman.com>
2010-12-20 23:47:15 +0200

    SetCookie parse/render

--- Web/Cookie.hs
+++ Web/Cookie.hs
@@ -1,29 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Web.Cookie
     ( -- * Server to client
-      {-SetCookie (..)
+      SetCookie (..)
     , parseSetCookie
     , renderSetCookie
       -- * Client to server
-    , -}Cookies
+    , Cookies
     , parseCookies
     , renderCookies
+      -- * Expires field
+    , expiresFormat
+    , formatCookieExpires
+    , parseCookieExpires
     ) where
 
 import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
 import Blaze.ByteString.Builder (Builder, fromByteString)
-import Blaze.ByteString.Builder.Char8 (fromChar)
-import Data.Monoid (mempty, mappend)
+import Blaze.ByteString.Builder.Char8 (fromChar, fromString)
+import Data.Monoid (mempty, mappend, mconcat)
 import Data.Word (Word8)
+import Data.Time (UTCTime, formatTime, parseTime)
+import System.Locale (defaultTimeLocale)
+import Data.Char (toLower)
+import Control.Arrow (first)
 
 type Cookies = [(S.ByteString, S.ByteString)]
 
--- | Decode the value of an HTTP_COOKIE header into key/value pairs.
+-- | Decode the value of a \"Cookie\" request header into key/value pairs.
 parseCookies :: S.ByteString -> Cookies
 parseCookies s
   | S.null s = []
   | otherwise =
-    let (first, rest) = breakDiscard 59 s -- semicolon
-     in parseCookie first : parseCookies rest
+    let (x, y) = breakDiscard 59 s -- semicolon
+     in parseCookie x : parseCookies y
 
 parseCookie :: S.ByteString -> (S.ByteString, S.ByteString)
 parseCookie s =
@@ -46,3 +56,61 @@ renderCookies cs =
 renderCookie :: (S.ByteString, S.ByteString) -> Builder
 renderCookie (k, v) =
     fromByteString k `mappend` fromChar '=' `mappend` fromByteString v
+
+data SetCookie = SetCookie
+    { setCookieName :: S.ByteString
+    , setCookieValue :: S.ByteString
+    , setCookiePath :: Maybe S.ByteString
+    , setCookieExpires :: Maybe UTCTime
+    , setCookieDomain :: Maybe S.ByteString
+    }
+    deriving (Eq, Show, Read)
+
+renderSetCookie :: SetCookie -> Builder
+renderSetCookie sc = mconcat
+    [ fromByteString $ setCookieName sc
+    , fromChar '='
+    , fromByteString $ setCookieValue sc
+    , case setCookiePath sc of
+        Nothing -> mempty
+        Just path -> fromByteString "; path=" `mappend` fromByteString path
+    , case setCookieExpires sc of
+        Nothing -> mempty
+        Just e -> fromByteString "; expires=" `mappend`
+                  fromString (formatCookieExpires e)
+    , case setCookieDomain sc of
+        Nothing -> mempty
+        Just d -> fromByteString "; domain=" `mappend` fromByteString d
+    ]
+
+parseSetCookie :: S.ByteString -> SetCookie
+parseSetCookie a = SetCookie
+    { setCookieName = key
+    , setCookieValue = value
+    , setCookiePath = lookup "path" pairs
+    , setCookieExpires =
+        lookup "expires" pairs >>= (parseCookieExpires . S8.unpack)
+    , setCookieDomain = lookup "domain" pairs
+    }
+  where
+    (key, value, b) = parsePair a
+    pairs = map (first $ S8.map toLower) $ parsePairs b
+    parsePair bs =
+        let (k, bs') = breakDiscard 61 bs -- equals sign
+            (v, bs'') = breakDiscard 59 bs' -- semicolon
+         in (k, v, S.dropWhile (== 32) bs'') -- space
+    parsePairs bs =
+        if S.null bs
+            then []
+            else let (k, v, bs') = parsePair bs
+                  in (k, v) : parsePairs bs'
+
+expiresFormat :: String
+expiresFormat = "%a, %d-%b-%Y %X GMT"
+
+-- | Format a 'UTCTime' for a cookie.
+formatCookieExpires :: UTCTime -> String
+formatCookieExpires = formatTime defaultTimeLocale expiresFormat
+
+parseCookieExpires :: String -> Maybe UTCTime
+parseCookieExpires = parseTime defaultTimeLocale expiresFormat
--- runtests.hs
+++ runtests.hs
@@ -8,18 +8,23 @@ import qualified Data.ByteString as S
 import qualified Data.ByteString.Lazy as L
 import Data.Word (Word8)
 import Control.Arrow ((***))
+import Control.Applicative ((<$>), (<*>))
+import Data.Time (UTCTime (UTCTime))
 
 main :: IO ()
 main = defaultMain
     [ testProperty "parse/render cookies" propParseRenderCookies
+    , testProperty "parse/render SetCookie" propParseRenderSetCookie
     ]
 
 propParseRenderCookies :: Cookies' -> Bool
 propParseRenderCookies cs' =
     parseCookies (builderToBs $ renderCookies cs) == cs
   where
-    cs = map (go *** go) cs'
-    go = S.pack . map unChar'
+    cs = map (fromUnChars *** fromUnChars) cs'
+
+fromUnChars :: [Char'] -> S.ByteString
+fromUnChars = S.pack . map unChar'
 
 builderToBs :: Builder -> S.ByteString
 builderToBs = S.concat . L.toChunks . toLazyByteString
@@ -31,3 +36,14 @@ instance Show Char' where
     showList = (++) . show . concatMap show
 instance Arbitrary Char' where
     arbitrary = fmap (Char' . toEnum) $ choose (62, 125)
+
+propParseRenderSetCookie :: SetCookie -> Bool
+propParseRenderSetCookie sc =
+    parseSetCookie (builderToBs $ renderSetCookie sc) == sc
+
+instance Arbitrary SetCookie where
+    arbitrary = SetCookie <$> (fmap fromUnChars arbitrary)
+                          <*> (fmap fromUnChars arbitrary)
+                          <*> (fmap (fmap fromUnChars) arbitrary)
+                          <*> fmap (parseCookieExpires . formatCookieExpires) (UTCTime <$> fmap toEnum arbitrary <*> return 0)
+                          <*> (fmap (fmap fromUnChars) arbitrary)
